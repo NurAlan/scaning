38219686f57e9fac9c2868364f0b608276ec11d2
NurAlan <wahyunuralan@gmail.com>
2018-08-12 10:33:47 +0700

    upload files

--- resources/views/frontend.blade.php
+++ resources/views/frontend.blade.php
@@ -8,7 +8,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>PT. </title>
+    <title>PT. Djarum</title>
 
     <!-- Bootstrap core CSS -->
     <link href="{{ asset('vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
